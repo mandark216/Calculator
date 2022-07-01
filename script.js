b3b6687189ae0f9876de49c78c585060a0c36b47
mandark216 <107990793+mandark216@users.noreply.github.com>
2022-07-01 17:56:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener('click', (e) => {
+        console.log(e.target)
+
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+
+
+        }
+
+         else if  (e.target.innerHTML == 'C') {
+            string = " ";
+            document.querySelector('input').value = string;
+
+        }
+        else {
+
+            string = string + e.target.innerHTML;
+            document.querySelector('.input').value = string;
+        }
+
+    })
+})
